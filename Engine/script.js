768fd8089c13af2578f781f7632d8dd09ab73d2f
Travis <tjxhahn@gmail.com>
2018-07-03 00:34:38 -0400

    Update script.js

--- Engine/script.js
+++ Engine/script.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+  $("#search").submit(function() {
+    //Call Search Function
+    /*
+    Search function will retrieve all the possiblities of the query from a database. This database will be created and maintained by the c++ file handling. The search function will then hide all of the current screen, and then show a page displaying all of the results.
+    
+    
+    */
+  });
+});
