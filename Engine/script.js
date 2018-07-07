61015cf1f0c4c9906409d86fc91c21285dac4436
Travis <tjxhahn@gmail.com>
2018-07-07 04:16:32 -0400

    Add files via upload

--- Engine/script.js
+++ Engine/script.js
@@ -1,10 +1,51 @@
+let results = [
+  {
+    link: 'https://www.google.com/',
+    keywords: ['google', 'search']
+  },
+  {
+    link: 'https://www.amazon.com/',
+    keywords: ['amazon', 'shop']
+  },
+  {
+    link: 'https://www.bing.com/',
+    keywords: ['bing', 'search', 'search engine']
+  }
+];
+
+
 $(document).ready(function() {
-  $("#search").submit(function() {
-    //Call Search Function
-    /*
-    Search function will retrieve all the possiblities of the query from a database. This database will be created and maintained by the c++ file handling. The search function will then hide all of the current screen, and then show a page displaying all of the results.
-    
-    
-    */
+  function search(input) {
+    let final = [];
+    for (let i = 0;i < results.length;i++) {
+      for (let j = 0;j < results[i].keywords.length;j++) {
+        if (results[i].keywords[j] == input) {
+          final.push(results[i].link);
+        }
+      }
+    }
+    return final;
+  }
+
+  $('#searchBar').on('keydown', function(e) {
+    if (e.which == 13) {
+      let links = search(document.getElementById('searchBar').value.toLowerCase().trim());      
+      if (links.length == 0) {
+        $('#spinner').show();
+        setTimeout(function() {
+          $('#spinner').hide();
+          $('section').hide();
+          $('#error').show();
+        }, 500);
+      } else {
+        $('#spinner').show();
+        setTimeout(function() {
+          $('#spinner').hide();
+          $('section').hide();
+          $('#results').show();
+          console.log(links);
+        }, 500);
+      }
+    }
   });
 });
